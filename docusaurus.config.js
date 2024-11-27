a469e9af765d8cae6d8528004e235e0b4c9dfa47
MacDuPain <godel.Sacha@gmail.com>
2024-11-27 10:28:23 +0100

    rajout du blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'THP Lab',
-  tagline: 'W3C is cool',
+  tagline: 'dinausors are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
